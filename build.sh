6d4c021d87d5abde9d5a58d4dce08a4d380c616e
ruither.borba <delki8@gmail.com>
2022-03-26 11:35:10 -0300

    updating build scripts and including front-end to docker-compose, also updating kafka listeners

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd ../pastorais
 sed -i 's/localhost\//db\//g' src/main/resources/META-INF/persistence.xml
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 && mvn clean install
 sed -i 's/db\//localhost\//g' src/main/resources/META-INF/persistence.xml
-cd ../docker || exit
-cp ../pastorais/target/pastorais-1.0-SNAPSHOT.war .
-docker build -f ./Dockerfile -t pastorais .
+cp ./target/pastorais-1.0-SNAPSHOT.war .
+docker build -f Dockerfile -t pastorais .
 rm ./pastorais-1.0-SNAPSHOT.war
+#cd ../docker || exit
