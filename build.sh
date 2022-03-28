83d2933d13b9acc42b1ce0a6dd8f1ab6e1858809
ruither.borba <delki8@gmail.com>
2022-03-28 07:48:52 -0300

    adding the comunicacao to build.sh and docker-compose

--- build.sh
+++ build.sh
@@ -8,3 +8,11 @@ cp ./target/pastorais-1.0-SNAPSHOT.war .
 docker build -f Dockerfile -t pastorais .
 rm ./pastorais-1.0-SNAPSHOT.war
 #cd ../docker || exit
+
+cd ../comunicacao
+sed -i 's/localhost:29092/kafka:9092/g' src/main/java/startup/App.java
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 && mvn clean install
+sed -i 's/kafka:9092/localhost:29092/g' src/main/java/startup/App.java
+cp ./target/comunicacao-1.0-SNAPSHOT.jar .
+docker build -f Dockerfile -t comunicacao .
+rm ./comunicacao-1.0-SNAPSHOT.jar
