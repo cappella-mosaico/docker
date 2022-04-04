4e7ad89b755f19f687fdf9b59609651182afc6ec
ruither.borba <delki8@gmail.com>
2022-04-04 08:24:15 -0300

    upgrading flyway url

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 cd ../pastorais
 sed -i 's/localhost\//db\//g' src/main/resources/META-INF/persistence.xml
+sed -i 's/localhost\//db\//g' src/main/java/startup/Startup.java
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 && mvn clean install
 sed -i 's/db\//localhost\//g' src/main/resources/META-INF/persistence.xml
+sed -i 's/db\//localhost\//g' src/main/java/startup/Startup.java
 cp ./target/pastorais-1.0-SNAPSHOT.war .
 docker build -f Dockerfile -t pastorais .
 rm ./pastorais-1.0-SNAPSHOT.war
