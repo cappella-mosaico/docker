a45288997c7dc79d54699c060a4ab90fa944d1d2
ruither.borba <delki8@gmail.com>
2022-04-18 18:56:47 -0300

    adding frontend build to script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# FRONTEND
+cd ../front-admin
+npm install
+
+# PASTORAIS
 cd ../pastorais
 sed -i 's/localhost\//db\//g' src/main/resources/META-INF/persistence.xml
 sed -i 's/localhost\//db\//g' src/main/java/startup/Startup.java
@@ -11,6 +16,7 @@ docker build -f Dockerfile -t pastorais .
 rm ./pastorais-1.0-SNAPSHOT.war
 #cd ../docker || exit
 
+# FINANCEIRO
 cd ../financeiro
 sed -i 's/localhost\//db-financeiro\//g' src/main/resources/META-INF/persistence.xml
 sed -i 's/localhost\//db-financeiro\//g' src/main/java/startup/Startup.java
@@ -20,6 +26,7 @@ sed -i 's/db-financeiro\//localhost\//g' src/main/java/startup/Startup.java
 cp ./target/financeiro-1.0-SNAPSHOT.war .
 docker build -f Dockerfile -t financeiro .
 
+# COMUNICACAO
 cd ../comunicacao
 sed -i 's/localhost:29092/kafka:9092/g' src/main/java/startup/App.java
 export ONE_SIGNAL_APP_ID="97bc067c-2344-4a86-a6b1-0206f51df4e9" && export ONE_SIGNAL_APP_KEY="NTgwOTNjYmEtNGZjYi00ZGYyLThmMTktMGM5MzdiNzRkZDkz" && JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 && mvn clean install
