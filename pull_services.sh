a2975f5ee51bda21a975dece19f6fc2d2ffbdf07
ruither.borba <delki8@gmail.com>
2022-05-26 19:05:35 -0300

    updating production docker compose and adding the new service to the pull_services script

--- pull_services.sh
+++ pull_services.sh
@@ -3,4 +3,5 @@ git clone git@github.com:cappella-mosaico/front-admin.git
 git clone git@github.com:cappella-mosaico/pastorais.git
 git clone git@github.com:cappella-mosaico/financeiro.git
 git clone git@github.com:cappella-mosaico/comunicacao.git
+git clone git@github.com:cappella-mosaico/eventos.git
 cd docker
