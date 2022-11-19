7c0c2329b87a9fc61a329be8bac282dc4087583e
ruither.borba <delki8@gmail.com>
2022-11-19 07:03:07 -0300

    update production docker-compose.yaml

--- linux_build.sh
+++ linux_build.sh
@@ -3,13 +3,28 @@
 echo 'dont forget to pass .prod as a param when building for production'
 
 # FRONTEND
-echo 'building frontend'
+echo ' _______  ______    _______  __    _  _______  _______  __    _  ______  '
+echo '|       ||    _ |  |       ||  |  | ||       ||       ||  |  | ||      | '
+echo '|    ___||   | ||  |   _   ||   |_| ||_     _||    ___||   |_| ||  _    |'
+echo '|   |___ |   |_||_ |  | |  ||       |  |   |  |   |___ |       || | |   |'
+echo '|    ___||    __  ||  |_|  ||  _    |  |   |  |    ___||  _    || |_|   |'
+echo '|   |    |   |  | ||       || | |   |  |   |  |   |___ | | |   ||       |'
+echo '|___|    |___|  |_||_______||_|  |__|  |___|  |_______||_|  |__||______| '
+
 cd ../front-admin
 npm install
 docker build --quiet --build-arg REACT_APP_IPMOSAICO_ROOT_URL=$REACT_APP_IPMOSAICO_ROOT_URL -f Dockerfile.prod -t front-admin-prod .
 
 # PASTORAIS
-echo 'building pastorais'
+
+echo ' _______  _______  _______  _______  _______  ______    _______  ___   _______ '
+echo '|       ||   _   ||       ||       ||       ||    _ |  |   _   ||   | |       |'
+echo '|    _  ||  |_|  ||  _____||_     _||   _   ||   | ||  |  |_|  ||   | |  _____|'
+echo '|   |_| ||       || |_____   |   |  |  | |  ||   |_||_ |       ||   | | |_____ '
+echo '|    ___||       ||_____  |  |   |  |  |_|  ||    __  ||       ||   | |_____  |'
+echo '|   |    |   _   | _____| |  |   |  |       ||   |  | ||   _   ||   |  _____| |'
+echo '|___|    |__| |__||_______|  |___|  |_______||___|  |_||__| |__||___| |_______|'
+
 cd ../docker
 cp ./tomcat/conf/web.xml$1 ../pastorais/web.xml
 cp ./tomcat/*.jar ../pastorais/
@@ -26,7 +41,14 @@ rm ./web.xml
 rm ./*.jar
 
 # FINANCEIRO
-echo 'building financeiro'
+echo ' _______  ___   __    _  _______  __    _  _______  _______  ___   ______    _______ '
+echo '|       ||   | |  |  | ||   _   ||  |  | ||       ||       ||   | |    _ |  |       |'
+echo '|    ___||   | |   |_| ||  |_|  ||   |_| ||       ||    ___||   | |   | ||  |   _   |'
+echo '|   |___ |   | |       ||       ||       ||       ||   |___ |   | |   |_||_ |  | |  |'
+echo '|    ___||   | |  _    ||       ||  _    ||      _||    ___||   | |    __  ||  |_|  |'
+echo '|   |    |   | | | |   ||   _   || | |   ||     |_ |   |___ |   | |   |  | ||       |'
+echo '|___|    |___| |_|  |__||__| |__||_|  |__||_______||_______||___| |___|  |_||_______|'
+
 cd ../docker
 cp ./tomcat/conf/web.xml$1 ../financeiro/web.xml
 cp ./tomcat/*.jar ../financeiro/
@@ -43,7 +65,14 @@ rm ./web.xml
 rm ./*.jar
 
 # COMUNICACAO
-echo 'building comunicacao'
+echo ' _______  _______  __   __  __   __  __    _  ___   _______  _______  _______  _______  _______ '
+echo '|       ||       ||  |_|  ||  | |  ||  |  | ||   | |       ||   _   ||       ||   _   ||       |'
+echo '|       ||   _   ||       ||  | |  ||   |_| ||   | |       ||  |_|  ||       ||  |_|  ||   _   |'
+echo '|       ||  | |  ||       ||  |_|  ||       ||   | |       ||       ||       ||       ||  | |  |'
+echo '|      _||  |_|  ||       ||       ||  _    ||   | |      _||       ||      _||       ||  |_|  |'
+echo '|     |_ |       || ||_|| ||       || | |   ||   | |     |_ |   _   ||     |_ |   _   ||       |'
+echo '|_______||_______||_|   |_||_______||_|  |__||___| |_______||__| |__||_______||__| |__||_______|'
+
 cd ../comunicacao
 sed -i 's/localhost:29092/kafka:9092/g' src/main/java/kafka/NewPastoralConsumer.java
 sed -i 's/localhost:29092/kafka:9092/g' src/main/java/kafka/LembretePastoralConsumer.java
@@ -57,7 +86,14 @@ docker build --quiet -f Dockerfile -t comunicacao .
 rm ./comunicacao-1.0-SNAPSHOT.jar
 
 # EVENTOS
-echo 'building eventos'
+echo ' _______  __   __  _______  __    _  _______  _______  _______ '
+echo '|       ||  | |  ||       ||  |  | ||       ||       ||       |'
+echo '|    ___||  |_|  ||    ___||   |_| ||_     _||   _   ||  _____|'
+echo '|   |___ |       ||   |___ |       |  |   |  |  | |  || |_____ '
+echo '|    ___||       ||    ___||  _    |  |   |  |  |_|  ||_____  |'
+echo '|   |___  |     | |   |___ | | |   |  |   |  |       | _____| |'
+echo '|_______|  |___|  |_______||_|  |__|  |___|  |_______||_______|'
+
 cd ../eventos
 sed -i 's/localhost:5432/db-eventos/g' src/main/resources/application.properties
 ./mvnw install -q
@@ -67,6 +103,13 @@ rm ./eventos-0.0.1-SNAPSHOT.jar
 sed -i 's/db-eventos/localhost:5432/g' src/main/resources/application.properties
 
 # GATEWAY
-echo 'building gateway'
+echo ' _______  _______  _______  _______  _     _  _______  __   __ '
+echo '|       ||   _   ||       ||       || | _ | ||   _   ||  | |  |'
+echo '|    ___||  |_|  ||_     _||    ___|| || || ||  |_|  ||  |_|  |'
+echo '|   | __ |       |  |   |  |   |___ |       ||       ||       |'
+echo '|   ||  ||       |  |   |  |    ___||       ||       ||_     _|'
+echo '|   |_| ||   _   |  |   |  |   |___ |   _   ||   _   |  |   |  '
+echo '|_______||__| |__|  |___|  |_______||__| |__||__| |__|  |___|  '
+
 cd ../gateway
 docker build --quiet -t gateway .
