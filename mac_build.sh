e6d314287816c1a8a5300eef65ff071a9472bd1a
ruither.borba <delki8@gmail.com>
2022-11-19 06:43:51 -0300

    applying quiet flag to the mac_build as well

--- mac_build.sh
+++ mac_build.sh
@@ -3,62 +3,70 @@
 echo 'dont forget to pass .prod as a param when building for production'
 
 # FRONTEND
+echo 'building frontend'
 cd ../front-admin
 npm install
-docker build --build-arg REACT_APP_IPMOSAICO_ROOT_URL=$REACT_APP_IPMOSAICO_ROOT_URL -f Dockerfile.prod -t front-admin-prod .
+docker build --quiet --build-arg REACT_APP_IPMOSAICO_ROOT_URL=$REACT_APP_IPMOSAICO_ROOT_URL -f Dockerfile.prod -t front-admin-prod .
 
 # PASTORAIS
+echo 'building pastorais'
 cd ../docker
 cp ./tomcat/conf/web.xml$1 ../pastorais/web.xml
 cp ./tomcat/*.jar ../pastorais/
 cd ../pastorais
 sed -i '' 's/localhost\//db\//g' src/main/resources/META-INF/persistence.xml
 sed -i '' 's/localhost\//db\//g' src/main/java/startup/Startup.java
-mvn clean install
+mvn --quiet clean install
 sed -i '' 's/db\//localhost\//g' src/main/resources/META-INF/persistence.xml
 sed -i '' 's/db\//localhost\//g' src/main/java/startup/Startup.java
 cp ./target/pastorais-1.0-SNAPSHOT.war .
-docker build -f Dockerfile -t pastorais .
+docker build --quiet -f Dockerfile -t pastorais .
 rm ./pastorais-1.0-SNAPSHOT.war
 rm ./web.xml
 rm ./*.jar
 
 # FINANCEIRO
+echo 'building financeiro'
 cd ../docker
 cp ./tomcat/conf/web.xml$1 ../financeiro/web.xml
 cp ./tomcat/*.jar ../financeiro/
 cd ../financeiro
 sed -i '' 's/localhost\//db-financeiro\//g' src/main/resources/META-INF/persistence.xml
 sed -i '' 's/localhost\//db-financeiro\//g' src/main/java/startup/Startup.java
-mvn clean install
+mvn --quiet clean install
 sed -i '' 's/db-financeiro\//localhost\//g' src/main/resources/META-INF/persistence.xml
 sed -i '' 's/db-financeiro\//localhost\//g' src/main/java/startup/Startup.java
 cp ./target/financeiro-1.0-SNAPSHOT.war .
-docker build -f Dockerfile -t financeiro .
+docker build --quiet -f Dockerfile -t financeiro .
 rm ./financeiro-1.0-SNAPSHOT.war
 rm ./web.xml
 rm ./*.jar
 
 # COMUNICACAO
+echo 'building comunicacao'
 cd ../comunicacao
 sed -i '' 's/localhost:29092/kafka:9092/g' src/main/java/kafka/NewPastoralConsumer.java
 sed -i '' 's/localhost:29092/kafka:9092/g' src/main/java/kafka/LembretePastoralConsumer.java
-export ONE_SIGNAL_APP_ID="97bc067c-2344-4a86-a6b1-0206f51df4e9" && export ONE_SIGNAL_APP_KEY="NTgwOTNjYmEtNGZjYi00ZGYyLThmMTktMGM5MzdiNzRkZDkz" && mvn clean install
+sed -i '' 's/localhost:29092/kafka:9092/g' src/main/java/kafka/NewRelatorioFinanceiroConsumer.java
+export ONE_SIGNAL_APP_ID="97bc067c-2344-4a86-a6b1-0206f51df4e9" && export ONE_SIGNAL_APP_KEY="NTgwOTNjYmEtNGZjYi00ZGYyLThmMTktMGM5MzdiNzRkZDkz" && mvn --quiet clean install
 sed -i '' 's/kafka:9092/localhost:29092/g' src/main/java/kafka/NewPastoralConsumer.java
 sed -i '' 's/kafka:9092/localhost:29092/g' src/main/java/kafka/LembretePastoralConsumer.java
+sed -i '' 's/kafka:9092/localhost:29092/g' src/main/java/kafka/NewRelatorioFinanceiroConsumer.java
 cp ./target/comunicacao-1.0-SNAPSHOT.jar .
-docker build -f Dockerfile -t comunicacao .
+docker build --quiet -f Dockerfile -t comunicacao .
 rm ./comunicacao-1.0-SNAPSHOT.jar
 
 # EVENTOS
+echo 'building eventos'
 cd ../eventos
 sed -i '' 's/localhost:5432/db-eventos/g' src/main/resources/application.properties
-./mvnw install
+./mvnw install -q
 cp ./target/eventos-0.0.1-SNAPSHOT.jar .
-docker build -f Dockerfile -t eventos .
+docker build --quiet -f Dockerfile -t eventos .
 rm ./eventos-0.0.1-SNAPSHOT.jar
 sed -i '' 's/db-eventos/localhost:5432/g' src/main/resources/application.properties
 
 # GATEWAY
+echo 'building gateway'
 cd ../gateway
-docker build -t gateway .
+docker build --quiet -t gateway .
