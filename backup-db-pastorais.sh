01f635d8b4f0e3e94e1d53e563ad3560350a670c
Ruither Borba <delki8@gmail.com>
2023-03-14 08:09:15 -0300

    preparing pastorais backup to be renamed

--- backup-db-pastorais.sh
+++ backup-db-pastorais.sh
@@ -1,2 +1,3 @@
+SERVICE_CONTAINER=$(docker ps -qf "name=docker_db-pastorais")
 SERVICE_NAME="pastorais"
-docker exec -it $1 bash -c 'pg_dump --user postgres pastorais > "$(date +'%Y-%m-%d_%H:%M')-backup.sql" && mv *.sql /tmp && tar czf backup.tar.gz tmp' && docker cp $1:backup.tar.gz ./"$(date +'%Y-%m-%d_%H%M')-backups-${SERVICE_NAME}.tar.gz"
+docker exec -it ${SERVICE_CONTAINER} bash -c 'pg_dump --user postgres pastorais > "$(date +'%Y-%m-%d_%H:%M')-backup.sql" && mv *.sql /tmp && tar czf backup.tar.gz tmp' && docker cp ${SERVICE_CONTAINER}:backup.tar.gz ./"$(date +'%Y-%m-%d_%H%M')-backups-${SERVICE_NAME}.tar.gz"
