bc218064ccf03c7d9f6141ac62b541585de79382
Ruither Borba <delki8@gmail.com>
2023-03-22 08:08:36 -0300

    backup scripts

--- backup-db-financeiro.sh
+++ backup-db-financeiro.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 SERVICE_CONTAINER=$(docker ps -qf "name=docker_db-financeiro")
 SERVICE_NAME="financeiro"
 docker exec -it ${SERVICE_CONTAINER} bash -c 'pg_dump --user postgres financeiro > "$(date +'%Y-%m-%d_%H:%M')-backup.sql" && mv *.sql /tmp && tar czf backup.tar.gz tmp' && docker cp ${SERVICE_CONTAINER}:backup.tar.gz ./"$(date +'%Y-%m-%d_%H%M')-backups-${SERVICE_NAME}.tar.gz"
