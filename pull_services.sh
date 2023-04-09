0bcdde664a0188d3e3ff85222aea93fa21f2d7b3
Ruither Borba <delki8@gmail.com>
2023-04-09 18:51:46 -0300

    re-adding gateway

--- pull_services.sh
+++ pull_services.sh
@@ -5,5 +5,6 @@ git clone git@github.com:cappella-mosaico/financeiro.git
 git clone git@github.com:cappella-mosaico/comunicacao.git
 git clone git@github.com:cappella-mosaico/eventos.git
 git clone git@github.com:cappella-mosaico/reverseproxy.git
+git clone git@github.com:cappella-mosaico/gateway.git
 git clone git@github.com:cappella-mosaico/compromissos.git
 cd docker
