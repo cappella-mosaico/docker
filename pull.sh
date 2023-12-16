a999e087e76a7076cdebb2fe93d4c9a94cc8e340
ruither <delki8@gmail.com>
2023-12-16 07:22:13 -0500

    updating instalation instructions

---
+++ pull.sh
@@ -0,0 +1,17 @@
+git pull &&
+    cd ../financeiro &&
+    git pull &&
+    cd ../compromissos &&
+    git pull &&
+    cd ../comunicacao &&
+    git pull &&
+    cd ../eventos &&
+    git pull &&
+    cd ../front-admin &&
+    git pull &&
+    cd ../gateway &&
+    git pull &&
+    cd ../pastorais &&
+    git pull &&
+    cd ../reverseproxy &&
+    git pull
