e0a874c4408467c6c7d881e4dc8a29ea4535b230
ruither.borba <delki8@gmail.com>
2023-12-18 09:13:26 -0300

    removing merged services

--- pull.sh
+++ pull.sh
@@ -1,17 +1,11 @@
 git pull &&
-    cd ../financeiro &&
-    git pull &&
     cd ../compromissos &&
-    git pull &&
-    cd ../comunicacao &&
     git pull &&
     cd ../eventos &&
     git pull &&
     cd ../front-admin &&
     git pull &&
     cd ../gateway &&
-    git pull &&
-    cd ../pastorais &&
     git pull &&
     cd ../reverseproxy &&
     git pull
--- pull_services.sh
+++ pull_services.sh
@@ -1,8 +1,5 @@
 cd ..
 git clone git@github.com:cappella-mosaico/front-admin.git
-git clone git@github.com:cappella-mosaico/pastorais.git
-git clone git@github.com:cappella-mosaico/financeiro.git
-git clone git@github.com:cappella-mosaico/comunicacao.git
 git clone git@github.com:cappella-mosaico/eventos.git
 git clone git@github.com:cappella-mosaico/reverseproxy.git
 git clone git@github.com:cappella-mosaico/gateway.git
